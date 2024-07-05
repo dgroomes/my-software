78a0aaa288ff884d93907ea521992d8b474642e5
David Groomes <dwgroomes@gmail.com>
2024-07-05 00:30:37 -0500

    Code to the convenient /opt/homebrew/opt symlink instead of Cellar

--- bash/bash-completion.bash
+++ bash/bash-completion.bash
@@ -7,15 +7,15 @@
 # We want to disable the eager-style loading of completion scripts (v1 era), so we set the BASH_COMPLETION_COMPAT_DIR
 # environment variable to a non-existent directory. We also need to load the compatibility script. For much more
 # information, see the notes in 'BASH_COMPLETION.md' in https://github.com/dgroomes/my-config.
+
 export BASH_COMPLETION_COMPAT_DIR="/disable-legacy-bash-completions-by-pointing-to-a-dir-that-does-not-exist"
 
 shopt -q progcomp
-dirs=(/opt/homebrew/Cellar/bash-completion@2/*)
-if [ ${#dirs[@]} -eq 0 ]; then
-    echo >&2 "No bash-completion@2 installation found. Completions will not be loaded."
-elif [ ${#dirs[@]} -gt 1 ]; then
-    echo >&2 "Multiple bash-completion@2 versions found in Homebrew directory. Please remove all but one. Completions will not be loaded."
+dir=/opt/homebrew/opt/bash-completion@2/
+
+if [[ -d $dir ]]; then
+    . "${dir}/share/bash-completion/bash_completion"
+    . "${dir}/etc/bash_completion.d/000_bash_completion_compat.bash"
 else
-    . "${dirs[0]}/share/bash-completion/bash_completion"
-    . "${dirs[0]}/etc/bash_completion.d/000_bash_completion_compat.bash"
+    echo >&2 "(warn) bash-completions not loaded."
 fi
