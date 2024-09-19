9eadf270ead44584d20ffc4d6f4120ad63519466
David Groomes <dwgroomes@gmail.com>
2024-09-18 23:11:30 -0500

    Node 20.17.0

--- Formula/my-node@20.rb
+++ Formula/my-node@20.rb
@@ -5,13 +5,13 @@ class MyNodeAT20 < Formula
 
   homepage "https://github.com/dgroomes/my-software"
 
-  url "https://nodejs.org/dist/v20.15.0/node-v20.15.0-darwin-arm64.tar.gz"
+  url "https://nodejs.org/dist/v20.17.0/node-v20.17.0-darwin-arm64.tar.gz"
 
   version "0.0.0"
 
   keg_only :versioned_formula
 
-  sha256 "2646c338d2c5ecabba4f745fc315d6fdfbb7e01b5badecc154ad27dda00325fc"
+  sha256 "476324108c4361935465631eec47df1c943ba2c87bc050853385b1d1c71f0b1f"
 
   def install
     libexec.mkpath
